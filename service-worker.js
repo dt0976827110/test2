4cf536d1df49f9d0d8567d705a963704d596d39f
dt0976827110 <dt0976827110@gmail.com>
2025-07-21 11:49:00 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,13 @@
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('v1').then(cache => {
-      return cache.addAll(['index.html']);
-    })
-  );
+  self.skipWaiting(); // 安裝完立刻啟用
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim()); // 立刻控制所有頁面
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
