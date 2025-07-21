d64de2c0da0a70fa1e819ef9be12f647b53cbc8b
dt0976827110 <dt0976827110@gmail.com>
2025-07-21 13:18:29 +0800

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD6srrR4Mg18cBDavmzJ4PDg4uFPPYPXbc",
+  authDomain: "superunion-pwa.firebaseapp.com",
+  projectId: "superunion-pwa",
+  storageBucket: "superunion-pwa.appspot.com",
+  messagingSenderId: "959968900132",
+  appId: "1:959968900132:web:4403e5ab3685ded0ae94b1",
+  measurementId: "G-YETG8ZKD1G"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/top.jpg'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
