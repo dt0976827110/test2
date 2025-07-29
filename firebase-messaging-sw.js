6c3fa08a17dd9dc2d9721638ee0b5f665df73201
dt0976827110 <dt0976827110@gmail.com>
2025-07-29 14:14:34 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,14 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
-firebase.initializeApp({
-  apiKey: "AIzaSyD6srrR4Mg18cBDavmzJ4PDg4uFPPYPXbc",
-  authDomain: "superunion-pwa.firebaseapp.com",
-  projectId: "superunion-pwa",
-  storageBucket: "superunion-pwa.appspot.com",
-  messagingSenderId: "959968900132",
-  appId: "1:959968900132:web:4403e5ab3685ded0ae94b1"
-});
+
 
 const messaging = firebase.messaging();
 messaging.onBackgroundMessage(function(payload) {
